84056406e0ec17f756e863524cea81fba67fc540
EC2 Default User <ec2-user@ip-172-31-24-17.us-east-2.compute.internal>
2020-09-05 23:59:15 +0000

    message

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,43 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum(arr.sort.last(2))
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).any? {|couple| sum(couple) == n}
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[bcdfghjklmnpqrstvwxyz]/i === s
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false if s.length==0 || s.match(/[^01]/);
+  return s.to_i % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = Float(price)
+  end
+  def price_as_string
+    format("$%.2f", @price)
+  end
 end
